0b1f871ad48d526a8d11f8cf12d751152a82b938
karl-wilson <okosechidelu@gmail.com>
2021-08-25 00:36:00 +0100

    add onClick to close lazyLoader

--- src/components/core/lazyloader/lazyloader.js
+++ src/components/core/lazyloader/lazyloader.js
@@ -1,7 +1,7 @@
 import style from './lazyloader.module.css'
 const Lazyloader = ({className, ...props}) => {
     return(
-        <div className={[className, style.load].join(' ')}>
+        <div className={[className, style.load].join(' ')} {...props}>
             <div className={style.innerwrapper}>
                 <p>{props.children}</p>
             </div>
--- src/pages/home.js
+++ src/pages/home.js
@@ -2,13 +2,36 @@ import HeadSection from "../components/layout/headSection/headSection"
 import MapSection from "../components/layout/mapSection/mapSection";
 import PageContainer from "../components/layout/pageContainer/pageContainer";
 import { Lazyloader } from "../components/core/core";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { useState, useEffect } from "react";
+import { searchbarActions } from "../store/store"; 
 const Home = props =>{
     const showload = useSelector(state=> state.loading)
     const error = useSelector(state=> state.error)
+    const {adderror, addloading} = searchbarActions
+    const dispatch = useDispatch();
+    const [On, setOn] = useState(false)
+    useEffect(() => {
+        if(showload){
+            setOn(true)
+        }else if(error){
+            setOn(true)
+        }else{
+            setOn(false)
+        }
+    }, [showload, error])
+    
+    const clickHandler = () =>{
+        if(On){
+        setOn(false)
+        dispatch(adderror(false))
+        dispatch(addloading(false))
+        }
+    }
+    
     return(
         <PageContainer>
-            { showload && <Lazyloader>{error? error:'Loading...'}</Lazyloader>}
+            { On && <Lazyloader id="lazyLoader" onClick={clickHandler}>{error? error:'Loading...'}</Lazyloader>}
             <HeadSection/>
             <MapSection/>
         </PageContainer>
